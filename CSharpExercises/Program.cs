e1de707daef38d0f367f0e681c4e570e3cf6856f
seancahall <seancahall@gmail.com>
2017-05-12 14:55:11 -0700

    fixing assert issue

--- CSharpExercises/Program.cs
+++ CSharpExercises/Program.cs
@@ -11,8 +11,12 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
+            /* Print Welcome Message */
+            Console.WriteLine("CSharpExercises");
+            Console.WriteLine();
+
             /* Test exercises (Uncomment tests to run them) (Ctrl + K,C to mass comment, Ctrl + K,U to mass uncomment) */
-            //Assert("HelloWorld() should return \"Hello World!\"", Exercises.HelloWorld() == "Hello World!");
+            Assert("HelloWorld() should return \"Hello World!\"", Exercises.HelloWorld() == "Hello World!");
             //Assert("SayHello(\"John\") should return \"Hello John!\"", Exercises.SayHello("John") == "Hello John!");
             //Assert("Sum(10,12) should return 22", Exercises.Sum(10, 12) == 22);
             //Assert("Divide(10,5) should return 2", Exercises.Divide(10, 5) == 2);
@@ -50,12 +54,37 @@ namespace CSharpExercises
             //Assert("CollatzConjecture(27) should return 111", Exercises.CollatzConjecture(27) == 111);
             //Assert("GetOccurences(\"Hello World!\", \"World\") should return 1", Exercises.GetOccurences("Hello World!", "World") == 1);
             //Assert("GetNext7Days() should return next 7 days", JsonConvert.SerializeObject(Exercises.GetNext7Days(DateTime.Parse("08/22/2016"))) == JsonConvert.SerializeObject(Constants.Next7Days));
-            //Assert("IsInLeapYear(2000) should return true", Exercises.IsLeapYear(2000) == true);
-            //Assert("IsInLeapYear(1904) should return true", Exercises.IsLeapYear(1904) == true);
-            //Assert("IsInLeapYear(1900) should return false", Exercises.IsLeapYear(1900) == false);
-            //Assert("IsInLeapYear(1901) should return false", Exercises.IsLeapYear(1901) == false);
-            //Assert("MortgageCalculator(300000, 3.8, 30, 12) should return 1397.87", Exercises.MortgageCalculator(300000, 3.8. 30, 12) == 1397.87);
+            //Assert("IsInLeapYear(2000) should return true", Exercises.IsInLeapYear(2000) == true);
+            //Assert("IsInLeapYear(1904) should return true", Exercises.IsInLeapYear(1904) == true);
+            //Assert("IsInLeapYear(1900) should return false", Exercises.IsInLeapYear(1900) == false);
+            //Assert("IsInLeapYear(1901) should return false", Exercises.IsInLeapYear(1901) == false);
+            //Assert("MortgageCalculator(300000, 3.8, 30, 12) should return 1397.87", Exercises.MortgageCalculator(300000, 3.8, 30, 12) == 1397.87);
             //Assert("DuckGoose(100) should print correctly", Exercises.DuckGoose(100) == Constants.DuckGooseResult);
+
+            /* Prompt user to exit */
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Asserts that a condition is true and prints a message to the console.
+        /// </summary>
+        /// <param name="exerciseName">The name of the exercise</param>
+        /// <param name="condition">The condition to assert</param>
+        static void Assert(string exerciseName, bool condition)
+        {
+            if (condition)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[PASS] - {exerciseName}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"[FAIL] - {exerciseName}");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
